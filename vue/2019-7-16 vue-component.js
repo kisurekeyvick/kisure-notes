634988ae39dd4bb00d0fbf03888f94387a2f309a
kisurekeyvick <zttaijue1207@163.com>
2019-07-18 21:02:12 +0800

    feat(vue/*)

---
+++ vue/2019-7-16 vue-component.js
@@ -0,0 +1,9 @@
+/** 
+ * https://www.jianshu.com/p/233d17dfbd16
+ * 
+ * Vue通用组件的封装
+ */
+
+/** 
+ * 
+ */
---
+++ vue/2019-7-19/vue-exp.vue
@@ -0,0 +1,16 @@
+<script>
+/**
+ * vue的相关项目经验总结：
+ * 
+ * https://www.cnblogs.com/lwming/p/11126398.html
+ * （1）关于页面有时候报错：
+ *  Error in mounted hook: "TypeError: handler.call is not a function"，
+ * 
+ *  这是因为，我们把一些生命周期的写法写错了，
+ *  错误写法：created: function() {}
+ *  正确写法：created() {}
+ * 
+ * (2)关于slot的使用：
+ *  
+ */
+</script>
